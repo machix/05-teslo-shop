1f224eaa7cb35f2606f95c83fdb8ed096dcdcfd9
machix <mmachix@gmail.com>
2022-07-08 23:31:49 -0300

    Productos en carrito

--- components/cart/CartList.tsx
+++ components/cart/CartList.tsx
@@ -1,5 +1,16 @@
-import { Typography } from "@mui/material";
+import NextLink from "next/link";
+
+import {
+  Typography,
+  Grid,
+  Link,
+  CardActionArea,
+  CardMedia,
+  Box,
+  Button,
+} from "@mui/material";
 import { initialData } from "../../database/products";
+import { ItemCounter } from "../ui";
 
 const productsInCart = [
   initialData.products[0],
@@ -11,7 +22,45 @@ export const CartList = () => {
   return (
     <>
       {productsInCart.map((product) => (
-        <Typography key={product.slug}>{product.title}</Typography>
+        <Grid container spacing={2} key={product.slug} sx={{ mb: 1 }}>
+          <Grid item xs={3}>
+            {/* Llevar a la pagina del producto */}
+            <NextLink href="/product/slug" passHref>
+              <Link>
+                <CardActionArea>
+                  <CardMedia
+                    image={`products/${product.images[0]}`}
+                    component="img"
+                    sx={{ borderRadius: "5px" }}
+                  />
+                </CardActionArea>
+              </Link>
+            </NextLink>
+          </Grid>
+          <Grid item xs={7}>
+            <Box display="flex" flexDirection="column">
+              <Typography variant="body1">{product.title}</Typography>
+              <Typography variant="body1">
+                Talla: <strong>M</strong>
+              </Typography>
+              {/*  Condicional */}
+              <ItemCounter />
+            </Box>
+          </Grid>
+          <Grid
+            item
+            xs={2}
+            display="flex"
+            alignItems="center"
+            flexDirection="column"
+          >
+            <Typography variant="subtitle1">{`$${product.price}`}</Typography>
+            {/* Editable */}
+            <Button variant="text" color="secondary">
+              Remover
+            </Button>
+          </Grid>
+        </Grid>
       ))}
     </>
   );
