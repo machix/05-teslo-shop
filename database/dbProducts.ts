6aaaa9c461651c6c0c736d7d77c2790a4ef5899d
Marcelo Mazar <mmachix@gmail.com>
2022-07-29 00:50:46 -0300

    search

--- database/dbProducts.ts
+++ database/dbProducts.ts
@@ -27,3 +27,29 @@ export const getAllProductSlugs = async (): Promise<ProductSlug[]> => {
 
   return slugs;
 };
+
+export const getProductsByTerm = async (term: string): Promise<IProduct[]> => {
+  term = term.toString().toLowerCase();
+
+  await db.connect();
+
+  const products = await Product.find({
+    $text: { $search: term },
+  })
+    .select("title images price slug inStock -_id")
+    .lean();
+
+  await db.disconnect();
+
+  return products;
+};
+
+export const getAllProducts = async (): Promise<IProduct[]> => {
+  await db.connect();
+
+  const products = await Product.find({}).lean();
+
+  await db.disconnect();
+
+  return JSON.parse(JSON.stringify(products));
+};
